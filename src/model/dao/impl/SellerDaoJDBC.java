f0831aa976c865c1adeb256543b55e2338f02d71
Diego Santana <3c.santana@gmail.com>
2024-03-21 10:58:49 -0300

    feat: implementacao do método finfById na classe SellerDaoJDBC

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
+	
+	private Connection conn;
+	
+	
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller obj) {
@@ -27,8 +41,41 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement preparedSt = null;
+		ResultSet result = null;
+		
+		try {
+			preparedSt = conn.prepareStatement(
+					"SELECT seller.*, department.Name AS DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.departmentId = department.Id "
+					+ "WHERE seller.Id = ?");
+			
+			preparedSt.setInt(1, id);
+			result = preparedSt.executeQuery();
+			
+			while (result.next()) {
+				Department department = new Department();
+				department.setId(result.getInt("DepartmentId"));
+				department.setName(result.getString("DepName"));
+				
+				Seller seller = new Seller();
+				seller.setId(id);
+				seller.setName(result.getString("Name"));
+				seller.setEmail(result.getString("Email"));
+				seller.setDate(result.getDate("BirthDate").toLocalDate());
+				seller.setBaseSalary(result.getDouble("BaseSalary"));
+				seller.setDepartment(department);
+				
+				return seller;
+			}
+			
+			return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
