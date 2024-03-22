57f556089c5eba0e71e50283332118698f018193
Diego Santana <3c.santana@gmail.com>
2024-03-21 21:48:09 -0300

    feat: Implementacao do metodo findByDepartment

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -101,6 +102,43 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement preparedSt = null;
+		ResultSet result = null;
+
+		
+		try {
+			preparedSt = conn.prepareStatement(
+					"SELECT seller.*, department.Name AS DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.departmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			preparedSt.setInt(1, department.getId());
+			result = preparedSt.executeQuery();
+			
+			List<Seller> sellers = new ArrayList<Seller>();
+			
+			while (result.next()) {
+				Seller seller = instantiateSeller(department, result);
+				sellers.add(seller);
+			}
+			
+			return sellers;
+			
+		} catch (SQLException e) {
+			
+			throw new DbException(e.getMessage());
+			
+		}finally {
+			DB.closeStatement(preparedSt);
+			DB.closeResultSet(result);
+		}
 	}
 
 }
